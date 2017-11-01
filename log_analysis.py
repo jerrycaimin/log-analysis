ca9cf1ced1fa58b8cbf5770f32033a2a289ad689
JerryCai <JerryCai@ADMINIB-IA85GL4>
2017-11-01 21:59:44 +0800

    update

--- log_analysis.py
+++ log_analysis.py
@@ -148,6 +148,12 @@ def _parse_issue(target_path, issues, warning_hittimes=None, define_times=None, 
 
         counter = 0
         advice = issue.get("advice", "")
+        if hasattr(advice,"get"):
+            advice_name = advice.get("@from", "")
+            if advice_name:
+                with open(advice_name) as adv_f:
+                    advice = adv_f.read()
+        
         issue_name = issue.get('@name', "")
         print("========Start to check issue%d: %s.==============\n" % (issue_num, issue_name))
         _write_log(output_file,
